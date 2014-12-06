b64d4ad79491b5e28f8beec2435d94778288418f
Sam Wang <sam.wang@nways.com.au>
2014-12-06 17:57:55 +1100

    add home direction switch

--- www/js/controllers/homeController.js
+++ www/js/controllers/homeController.js
@@ -4,31 +4,42 @@ gdayModule.controller('homeController', ['$scope', 'homeResource', function($sco
 
 	$('.bar-header').addClass('bar-transparent');
 
-    homeResource.getNextTrainByDirection(0).success(function(response){
+    $scope.direction = 1;
 
-        if(response.errorCode > 0){
-            //TODO handle error
-        }else{
-            $scope.trains = response.result;
-            //var a = moment().format("H:mm:ss");
-            var now = moment();
-           // var arriveTime = $scope.trains.nextTrain.arriveTime;
-            var arriveAt = moment($scope.trains.nextTrain.arriveTime, "HH:mm:ss");
-            var diff = arriveAt.diff(now, 'seconds');
-            if(diff < 0) {
-                arriveAt = arriveAt.add(1, 'days');
-                diff = arriveAt.diff(now, 'seconds');
+    $scope.switchDirection = function(){
+        $scope.direction = 1 - $scope.direction;
+
+        loadHome();
+    }
+
+    function loadHome(){
+        homeResource.getHome($scope.direction).success(function(response){
+
+            if(response.errorCode > 0){
+                //TODO handle error
+            }else{
+                $scope.trains = response.result;
+                //var a = moment().format("H:mm:ss");
+                var now = moment();
+                // var arriveTime = $scope.trains.nextTrain.arriveTime;
+                var arriveAt = moment($scope.trains.nextTrain.arriveTime, "HH:mm:ss");
+                var diff = arriveAt.diff(now, 'seconds');
+                if(diff < 0) {
+                    arriveAt = arriveAt.add(1, 'days');
+                    diff = arriveAt.diff(now, 'seconds');
+                }
+                var minutesToGo = Math.ceil(diff / 60);
+                var hoursToGo = 0;
+                if(minutesToGo >= 60){
+                    hoursToGo = Math.floor(minutesToGo / 60);
+                    minutesToGo = minutesToGo % 60;
+                }
+                $scope.arriveInHours = hoursToGo;
+                $scope.arriveInMins = minutesToGo;
+
             }
-            var minutesToGo = Math.ceil(diff / 60);
-            var hoursToGo = 0;
-            if(minutesToGo >= 60){
-                hoursToGo = Math.floor(minutesToGo / 60);
-                minutesToGo = minutesToGo % 60;
-            }
-            $scope.arriveInHours = hoursToGo;
-            $scope.arriveInMins = minutesToGo;
+        });
+    }
 
-        }
-    });
-
+    loadHome();
 }]);
