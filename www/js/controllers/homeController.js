c74d17fd9ab3294a45c2efb33bed0b4e0d1f7ea6
Sam Wang <sam.wang@nways.com.au>
2014-12-07 17:48:47 +1100

    refine the train arrival count down

--- www/js/controllers/homeController.js
+++ www/js/controllers/homeController.js
@@ -1,73 +1,95 @@
 'use strict';
 
-gdayModule.controller('homeController', ['$scope', '$rootScope', 'homeResource', 'userService', function($scope, $rootScope, homeResource, userService) {
+gdayModule.controller('homeController',
+    ['$scope',
+        '$rootScope',
+        '$interval',
+        'homeResource',
+        'userService', function($scope, $rootScope, $interval, homeResource, userService) {
+
+    var countDownInterval;
 
 	$('.bar-header').addClass('bar-transparent');
 
     $scope.isLoading = false;
 
-    $scope.toCity = $rootScope.user == null ? 1 : $rootScope.user.toCity;
+    $scope.train = {
+        toCity: $rootScope.user == null ? 1 : $rootScope.user.toCity,
+        destination: '',
+        delay: '',
+        arriveHour : '',
+        arriveMinute : '',
+        arriveAmPm : '',
+        hoursToGo: '',
+        minutesToGo: ''
+    }
 
     $scope.switchDirection = function(){
         if($scope.isLoading){
             return;
         }
 
-        $scope.toCity = 1 - $scope.toCity;
+        $scope.train.toCity = 1 - $scope.train.toCity;
 
-        userService.saveTrainDirection($rootScope.user, $scope.toCity);
+        userService.saveTrainDirection($rootScope.user, $scope.train.toCity);
 
         loadHome();
     }
 
     function loadHome(){
         $scope.isLoading = true;
-        homeResource.getHome($scope.toCity).success(function(response){
+
+        homeResource.getHome($scope.train.toCity).success(function(response){
             $scope.isLoading = false;
             if(response.errorCode > 0){
                 //TODO handle error
             }else{
-                $scope.trains = response.result;
-                $scope.arrive = $scope.trains.nextTrain.arriveTime;
-                $scope.hh = ($scope.arrive).split(":")[0];
-                $scope.mm = ($scope.arrive).split(":")[1];
+                var nextTrain = response.result.nextTrain;
+                $scope.train.destination = nextTrain.destination;
 
-                var now = moment();
-
-                var arriveAt = moment($scope.arrive, "HH:mm:ss");
-                var diff = arriveAt.diff(now, 'seconds');
-                if(diff < 0) {
-                    arriveAt = arriveAt.add(1, 'days');
-                    diff = arriveAt.diff(now, 'seconds');
+                var arriveTime = nextTrain.arriveTime;
+                if(arriveTime == null){
+                    return;
                 }
-                var minutesToGo = Math.ceil(diff / 60);
-                var hoursToGo = 0;
-                if(minutesToGo >= 60){
-                    hoursToGo = Math.floor(minutesToGo / 60);
-                    minutesToGo = minutesToGo % 60;
-                }
-                var pad = '00';
 
-                $scope.arriveInHours = (pad + hoursToGo).slice(-pad.length);
-                $scope.arriveInMins = (pad + minutesToGo).slice(-pad.length);
+                var arriveAt = moment(arriveTime, "HH:mm:ss");
 
-                if($scope.trains.nextTrain.delay) {
-                    $scope.delay = $scope.trains.nextTrain.delay + "Mins Delay"
+                $scope.train.arriveHour = arriveAt.format('HH');
+                $scope.train.arriveMinute = arriveAt.format('mm');
+                $scope.train.arriveAmPm = arriveAt.format('A');
+
+                if(nextTrain.delay > 0) {
+                    var delayCopy = 'Min Delay';
+                    if(nextTrain.delay > 1){
+                        delayCopy = 'Mins Delay';
+                    }
+                    $scope.train.delay = nextTrain.delay + delayCopy;
                 } else {
-                    $scope.delay = "Running On Time"
+                    $scope.train.delay = "Running On Time"
                 }
 
+                calculateTimeToGo(arriveAt);
+                startCountdown(arriveAt);
             }
         });
     }
-    function updateTime(){
+
+    function calculateTimeToGo(arriveAt){
         var now = moment();
-        var arriveAt = moment($scope.arrive, "HH:mm:ss");
         var diff = arriveAt.diff(now, 'seconds');
-        if(diff < 0) {
+
+        //if diff > 10 hours, we can guess the arrive time is in the next day
+        //otherwise, we will pull the new arrival time
+
+        if(diff < -(3600 * 10)) {
             arriveAt = arriveAt.add(1, 'days');
             diff = arriveAt.diff(now, 'seconds');
         }
+        else if(diff < 0){
+            loadHome();
+            return;
+        }
+
         var minutesToGo = Math.ceil(diff / 60);
         var hoursToGo = 0;
         if(minutesToGo >= 60){
@@ -76,17 +98,37 @@ gdayModule.controller('homeController', ['$scope', '$rootScope', 'homeResource',
         }
         var pad = '00';
 
-        $scope.arriveInHours = (pad + hoursToGo).slice(-pad.length);
-        $scope.arriveInMins = (pad + minutesToGo).slice(-pad.length);
-        console.log($scope.arriveInMins);
+        $scope.train.hoursToGo = (pad + hoursToGo).slice(-pad.length);
+        $scope.train.minutesToGo = (pad + minutesToGo).slice(-pad.length);
     }
 
+    function startCountdown(arriveAt){
+        //Stop old count down before starting a new one
+        stopCountdown();
+
+        countDownInterval = $interval(function(){
+            calculateTimeToGo(arriveAt);
+        }, 5000);
+    }
+
+    function stopCountdown() {
+        if (angular.isDefined(countDownInterval)) {
+            $interval.cancel(countDownInterval);
+            countDownInterval = undefined;
+        }
+    };
 
     $scope.doRefresh = function() {
         loadHome();
         $scope.$broadcast('scroll.refreshComplete');
     }
 
+    $scope.$on('$destroy', function() {
+        // Make sure that the interval is destroyed too
+        stopCountdown();
+
+        console.log('countdown destroy');
+    });
+
     loadHome();
-    setInterval(function(){updateTime()},1000);
 }]);
